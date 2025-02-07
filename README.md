dfa6c676212f1ceebe4834bff3bb20156099f8bc
Garrett Van Neste <garrett56x@gmail.com>
2025-02-06 16:03:27 -0800

    Added randomization to click location

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gemClicker
+
+Start venv:
+source venv/bin/activate
+
+Run app:
+python index.py
--- index.py
+++ index.py
@@ -5,6 +5,8 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import random
+from datetime import datetime
+import pytz
 
 # Game window values
 screen_x = 0
@@ -19,16 +21,27 @@ screen_height = 915
 # screen_height = 75
 
 # Load the button reference image
-button_image_path = "./button.png"
+button_image_path = "./img/button.png"
 button_image = Image.open(button_image_path)
-count = 0
+# Load all floater images
+floater_image_paths = [f"./img/floater_{direction}.png" for direction in ["n", "ne", "e", "se", "s", "sw", "w", "nw"]]
+floater_images = [Image.open(path) for path in floater_image_paths]
+
+button_total = 0
+floater_total = 0
+
+pst = pytz.timezone('America/Los_Angeles')
+
+# click window to focus
+# pyautogui.click(100,500)
 
 # Function to capture the screen and look for the button
 def find_and_click_button():
-    global count
+    global button_total
+    global floater_total
     # Take a screenshot of the game area
     screenshot = pyautogui.screenshot(region=(screen_x, screen_y, screen_width, screen_height))
-    screenshot.save("screenshot.png")
+    # screenshot.save("screenshot.png")
     screenshot = np.array(screenshot)  # Convert to NumPy array (OpenCV uses this format)
 
     # Convert button image to NumPy array
@@ -47,14 +60,66 @@ def find_and_click_button():
         click_x = screen_x + button_x + button_image.width // 2
         click_y = screen_y + button_y + button_image.height // 2
 
-        pyautogui.moveTo(click_x, click_y, duration=0.1)
-        pyautogui.doubleClick()
+        offset_x = random.randint(-5, 5)
+        offset_y = random.randint(-5, 5)
 
-        count += 5
+        click_x += offset_x
+        click_y += offset_y
+
+        pyautogui.moveTo(click_x, click_y, duration=0.1)
+        pyautogui.click()
+
+        # account for window not being focused i.e. extra click
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        if max_val > 0.85:
+            time.sleep(1)
+            click_x -= offset_x
+            click_y -= offset_y
+
+            offset_x = random.randint(-5, 5)
+            offset_y = random.randint(-5, 5)
+
+            click_x += offset_x
+            click_y += offset_y
+
+            pyautogui.moveTo(click_x, click_y, duration=0.1)
+            pyautogui.click()
+
+        button_total += 5
+        print(f"{button_total + floater_total} gems collected so far!")
+
         return True
     else:
         print("Button not found.")
         return False
+    
+# Function to capture the screen and look for the button
+def find_and_click_floater():
+    global button_total
+    global floater_total
+    screenshot = pyautogui.screenshot(region=(screen_x, screen_y, screen_width, screen_height))
+    screenshot = np.array(screenshot)  
+
+    for image in floater_images:
+        image_np = np.array(image)
+        result = cv2.matchTemplate(screenshot, image_np, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+        if 0.4 < max_val < 0.55:
+            return "gem"
+        elif max_val > 0.55:  # Confidence threshold
+            click_x = screen_x + max_loc[0] + image.width // 2
+            click_y = screen_y + max_loc[1] + image.height // 2
+
+            pyautogui.moveTo(click_x, click_y, duration=0.1)
+            pyautogui.click()
+
+            floater_total += 2
+            print(f"{button_total + floater_total} gems collected so far!")
+
+            return "clicked"
+
+    return False
 
 try:
     while True:
@@ -67,32 +132,32 @@ try:
             # If the button was NOT found, retry after 1-2 minutes
             sleep_time = random.randint(1 * 60, 2 * 60)
 
-        print(f"Sleeping for {sleep_time / 60:.2f} minutes before the next check.")
-
-        if count % 5 == 0:
-            print(f"{count} gems collected so far!")
+        current_time = datetime.now(pst).strftime("%I:%M %p")
+        print(f"[{current_time}] Sleeping for {sleep_time / 60:.2f} minutes before the next check.")
 
         time.sleep(sleep_time)
 
+# except KeyboardInterrupt:
+#     print(f"\n")
+
+# try:
+#     while True:
+#         # clicked = find_and_click_floater()
+
+#         if clicked == "clicked":
+#             sleep_time = random.randint(15 * 60, 17 * 60)
+#         elif clicked == "gem":
+#             sleep_time = random.randint(1, 2)
+#         else:
+#             sleep_time = random.randint(1 * 60, 2 * 60)
+
+#         time.sleep(sleep_time)
+
 except KeyboardInterrupt:
-    # Convert count to an integer to remove decimals
-    count_int = int(count)
-
-    # Calculate the width of the total line
-    count_length = len(str(count_int))
-    line_length = 55  # Adjust this based on how wide you want the border to be
-    message = f"***  Automation complete. Total gems collected: {count_int}!  ***"
-    message_length = len(message)
-
-    # Calculate the padding on both sides to center the message
-    padding_length = (line_length - message_length) // 2
-
-    # Create the formatted lines
-    border_line = "*" * line_length
-    padded_message_line = " " * padding_length + message
-
-    # Print the message with the border
-    print(f"\n\n{border_line}")
-    print(padded_message_line)
-    print(border_line)
-    print("\n\n")
+    print(f"\n\n*********************************************")
+    print(f"*********************************************\n")
+    print(f"Automation complete. Gems collected: {button_total + floater_total}.")
+    # print(f"Gems from buttons: {button_total}.")
+    # print(f"Gems from floaters: {floater_total}.")
+    print(f"\n*********************************************")
+    print(f"*********************************************\n\n")
